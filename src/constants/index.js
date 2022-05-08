a18e44ab437a43bd56d5c398ad3f1f1f6f802c72
thaiqn-it <qthai20102000@gmail.com>
2022-05-09 01:34:01 +0700

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 const API_SUCCSES = 200;
 const API_BAD_REQUEST = 400;
 const API_URL = 
-"https://studentloanfpt.ddns.net/api"
+"http://localhost:3000/api"
 //  "http://localhost:3000/api";
 
 const PRIMARY_COLOR_WHITE = "#ffffff";
